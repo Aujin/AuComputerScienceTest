19623a77caf70d547224709b9e6b5cf52d11bdf2
Aujin <jinxuran@aurumjin.cn>
2023-06-01 22:39:13 +0800

    Add Seq List Function

---
+++ common.h
@@ -0,0 +1,10 @@
+//
+// Created by JXR on 2023/6/3.
+//
+
+#ifndef AUCOMPUTERSCIENCETEST_COMMON_H
+#define AUCOMPUTERSCIENCETEST_COMMON_H
+
+typedef int ElemType;           //数据类型
+
+#endif //AUCOMPUTERSCIENCETEST_COMMON_H
---
+++ main.c
@@ -0,0 +1,9 @@
+
+#include "stdio.h"
+#include "algorithm_datastructure/list/seqlist.h"
+
+int main() {
+    printf("Hello World!\n");
+    TestSeqList();
+    return 0;
+}
